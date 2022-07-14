0f0f69777cb7f49a3c4b0bc52e3c4199f7c37f33
PearAmp <pearamp1999@gmail.com>
2022-07-14 17:36:23 +0700

    add

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import SmartCare from './components/home/SmartCare'
+
+const App = () => {
+  return (
+    <View>
+      <SmartCare />
+    </View>
+  )
+}
+
+export default App;
---
+++ components/home/SmartCare.js
@@ -0,0 +1,116 @@
+import {View, Text, Image, StyleSheet, ScrollView, SafeAreaView} from 'react-native';
+import React from 'react';
+
+export default function SmartCare() {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+       <SafeAreaView>
+       <View style={styles.imagesIcon}>
+          <Image
+            style={{width: 30, height: 30}}
+            source={require('../../assets/icons/hamburger.png')}
+          />
+          <Image
+            style={{width: 30, height: 30}}
+            source={require('../../assets/icons/bell.png')}
+          />
+        </View>
+       </SafeAreaView>
+        <View style={{marginTop: 10}}>
+          <Text style={styles.headerText}>SMART CARE</Text>
+        </View>
+
+        <View style={styles.greenBox}>
+         <View style={styles.Boxwrap}>
+         <View style={styles.textBox}>
+            <Text style={styles.titleText}>ประเมินความเสี่ยง</Text>
+            <Text style={styles.textContent}>
+              พร้อมรับ QR code การประเมิน
+            </Text>
+            <Text style={styles.textContent}>
+              สำหรับตรวจสอบระดับความเสี่ยง
+            </Text>
+            <Text style={styles.textContent}>
+              ของพนักงาน
+            </Text>
+          </View>
+          <View style={styles.Imageright}>
+          <Image style={{width: 130, height: 130}} source={require('../../assets/images/doctor.png')} />
+
+          </View>
+
+         </View>
+        </View>
+
+        <View style={styles.bannerwrap}>
+            <View style={[styles.risk]}>
+            <Text style={{textAlign: 'center', fontSize: 15, fontWeight: '500', color: '#000'}}>ระดับความเสี่ยงฉัน</Text>
+            <Text style={{textAlign: 'center', fontSize: 20, fontWeight: '700', color: '#000', marginTop: 10,}}>เสี่ยงต่ำ</Text>
+            <View style={{flexDirection: 'row', justifyContent: 'flex-end', marginTop: 30,}}>
+            <Image style={{width: 50, height: 50,}} source={require('../../assets/images/graph.png')} />
+            </View>
+            </View>
+            <View style={styles.warn}>
+            <Image style={{width: 130, height: 130}} source={require('../../assets/images/warn.png')} />
+            <Text style={{textAlign: 'center', fontSize: 15, fontWeight: '500', color: '#000'}}>ระบบการแจ้งเตือน</Text>
+            </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 12,
+    marginTop: 10,
+  },
+  imagesIcon: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  headerText: {
+    color: '#000',
+    fontSize: 15,
+    fontWeight: '500',
+  },
+  greenBox: {
+    marginTop: 10,
+    backgroundColor: '#1d9a24',
+    paddingHorizontal: 10,
+    borderRadius: 30,
+    paddingVertical: 20,
+  },
+  titleText: {
+    paddingVertical: 10,
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: '500',
+  },
+  textContent: {
+    color: '#fff',
+
+  },
+  Boxwrap: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  bannerwrap: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 30,
+  },
+  risk: {
+    backgroundColor: '#ffcf4e',
+    borderRadius: 20,
+    paddingHorizontal: 25,
+    paddingVertical: 20,
+  },
+  warn: {
+    backgroundColor: '#e4e4e4',
+    borderRadius: 20,
+    paddingHorizontal: 25,
+    paddingVertical: 20,
+  }
+
+});
